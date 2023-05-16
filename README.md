5d69fa85f0e3607a36179415d366a9334c6f4147
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-05-16 16:23:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# **Arrays**
+
+## Objective
+---
+<br>
+
+- Write a program that repeatedly prompts the user to supply scores (out of 10) on a test. The program should continue to ask the user for marks until a negative value is supplied. Any values greater than ten should be ignored. Once the program has read all the scores, it should produce a table with the following headings:
+  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;```Score``` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   ```# of Occurrences```
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The program should then calculate the mean score, rounded to one &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;decimal place.
+
+- Has In.java
+- Used Java version 1.2 ~~(*is updated though*)~~
+
+<br></br>
+
+***Made on May 10th, 2023***\
+***By: Aaron Prince Anu***
+
+
+<sub>*Temporary?*</sub>
---
+++ arrays.java
@@ -0,0 +1,68 @@
+public class arrays {
+
+    //initialize vars
+    static int score;
+    static int[] markScores;
+    static double PRECISION = 10.0;
+    static int totalOccurrences;
+
+
+    public static void main(String[] args) {
+
+        //initial array capacity
+        final int INITIAL_CAPACITY = 11;
+
+        //initial array
+        markScores = new int[INITIAL_CAPACITY];
+        totalOccurrences = 0;
+
+        //ask for scores
+        while (true) {
+            System.out.print("\nEnter a score (or a negative value to exit): ");
+            score = In.getInt();
+
+            //exit loop
+            if (score < 0) {
+                break;
+            }
+
+            //ignore values over 11
+            if (score < INITIAL_CAPACITY) {
+                // Expand the scores array if it gets full
+                if (totalOccurrences == markScores.length) {
+                    markScores = expandArray(markScores);
+                }
+
+                markScores[score]++;
+                totalOccurrences++;
+            }
+        }
+
+        //print table headers
+        System.out.println("\nScore\t\t# of Occurrences");
+
+        //calculate average
+        double sum = 0.0;
+        double mean;
+        for (int i = 0; i < markScores.length; i++) {
+            if (markScores[i] != 0) {
+                System.out.println(i + "\t\t" + markScores[i]);
+                sum += i * markScores[i];
+            }
+        }
+        mean = sum / totalOccurrences;
+
+        // print average
+        System.out.println("\nMean score: " + Math.round(mean * PRECISION) / PRECISION + "\n");
+    }
+
+    //expand array
+    private static int[] expandArray(int[] array) {
+        int newCapacity = array.length * 2;
+        int[] newArray = new int[newCapacity];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        return newArray;
+    }
+}
